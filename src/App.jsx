104cf21058bd69ca40be949cc7708a78201eb036
NeslihanUTUK <nesli.utuk@gmail.com>
2026-06-03 11:23:41 +0200

    project is aangemaakt

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Alert } from "@rijkshuisstijl-community/components-react";
+
+function App() {
+  <Alert type="info">
+    <Heading level={3}>Heading</Heading>
+    <Paragraph>
+      Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod
+      *
+    </Paragraph>
+  </Alert>;
+}
+
+export default App;
