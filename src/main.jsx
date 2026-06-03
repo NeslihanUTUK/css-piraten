34b373ab5485172a34c614c3cce3f7fdb0ed15d9
NeslihanUTUK <nesli.utuk@gmail.com>
2026-06-03 13:30:04 +0200

    fieat: voorbeeld pagina is klaar

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 
-createRoot(document.querySelector('#root')).render(
+createRoot(document.querySelector('#app')).render(
   <StrictMode>
     <App />
   </StrictMode>
